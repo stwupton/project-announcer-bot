4ffa1ad68691f04bd3e9e33abf4fce50301bd29d
Steven Upton <supton.sz@gmail.com>
2020-05-02 21:53:28 +0100

    Added help message for complete command

--- src/messages/help.ts
+++ src/messages/help.ts
@@ -6,6 +6,10 @@ export default `
 \`pab track "[name]" "[description]" [dd]/[mm]/[yyyy]\`
 *Note: the reminders will be sent to the same channel that this command is sent to.*
 
+**Complete Project**
+\`pab complete [index]\`
+*Note: run \`pab list\` to see project indexes.*
+
 **Delete Project**
 \`pab delete [index]\` 
 *Note: run \`pab list\` to see project indexes.*
